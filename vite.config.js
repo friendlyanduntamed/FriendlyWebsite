68c5692628b8268991bd223a59001c018a13326d
Friendly and Untamed <friendlyanduntamed@gmail.com>
2026-01-22 11:44:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-
 export default defineConfig({
   root: './',
   publicDir: 'public',
@@ -12,9 +11,6 @@ export default defineConfig({
         about: resolve(__dirname, 'about.html'),
         portfolio: resolve(__dirname, 'portfolio.html'),
         blog: resolve(__dirname, 'blog.html'),
-        blogRanthambore: resolve(__dirname, 'blog/ranthambore.html'),
-        blogAssam: resolve(__dirname, 'blog/assam.html'),
-        blogLadakh: resolve(__dirname, 'blog/ladakh.html'),
         contact: resolve(__dirname, 'contact.html'),
         shop: resolve(__dirname, 'shop.html'),
       },
